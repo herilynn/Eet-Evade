e988281bfc9728da6b8b022b0d120af362d9e26b
herilynn <4henrylin17@gmail.com>
2023-05-08 13:53:46 -0400

    Fixed sprite sizing and replaced squares with sprites

--- script.js
+++ script.js
@@ -21,30 +21,62 @@ Item.prototype.update = function() {
 }
 
 Item.prototype.draw = function() {
+  let razzberry = new Image();
+  razzberry.src = "assets/razzberry.png";
+
+  let firestone = new Image();
+  firestone.src = "assets/firestone.png";
+
+  let waterstone = new Image();
+  waterstone.src = "assets/waterstone.png";
+
+  let thunderstone = new Image();
+  thunderstone.src = "assets/thunderstone.png";
+
+  let pokeball = new Image();
+  pokeball.src = "assets/pokeball.png";
+
+  let greatball = new Image();
+  greatball.src = "assets/greatball.png";
+
+  let ultraball = new Image();
+  ultraball.src = "assets/ultraball.png";
+
+  let masterball = new Image();
+  masterball.src = "assets/masterball.png"
+
   if (this.type === 'berry') {
-    ctx.fillStyle = 'black';
-    ctx.fillRect(this.x, this.y, 20, 20);
+    ctx.drawImage(razzberry, this.x, this.y, 30, 30);
+    // ctx.fillStyle = 'black';
+    // ctx.fillRect(this.x, this.y, 20, 20);
   } else if (this.type === 'fire') {
-    ctx.fillStyle = 'orange';
-    ctx.fillRect(this.x, this.y, 20, 20);
+    // ctx.fillStyle = 'orange';
+    // ctx.fillRect(this.x, this.y, 20, 20);
+    ctx.drawImage(firestone, this.x, this.y, 30, 30);
   } else if (this.type === 'thunder') {
-    ctx.fillStyle = 'green';
-    ctx.fillRect(this.x, this.y, 20, 20);
+    // ctx.fillStyle = 'green';
+    // ctx.fillRect(this.x, this.y, 20, 20);
+    ctx.drawImage(thunderstone, this.x, this.y, 25, 25);
   } else if (this.type === 'water') {
-    ctx.fillStyle = 'purple';
-    ctx.fillRect(this.x, this.y, 20, 20);
+    // ctx.fillStyle = 'purple';
+    // ctx.fillRect(this.x, this.y, 20, 20);
+    ctx.drawImage(waterstone, this.x, this.y, 35, 35);
   } else if (this.type === 'poke') {
-    ctx.fillStyle = 'red';
-    ctx.fillRect(this.x, this.y, 20, 20);
+    // ctx.fillStyle = 'red';
+    // ctx.fillRect(this.x, this.y, 20, 20);
+    ctx.drawImage(pokeball, this.x, this.y, 20, 20);
   } else if (this.type === 'great') {
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(this.x, this.y, 20, 20);
+    // ctx.fillStyle = 'blue';
+    // ctx.fillRect(this.x, this.y, 20, 20);
+    ctx.drawImage(greatball, this.x, this.y, 30, 30);
   } else if (this.type === 'ultra') {
-    ctx.fillStyle = 'yellow' 
-    ctx.fillRect(this.x, this.y, 20, 20);
+    // ctx.fillStyle = 'yellow' 
+    // ctx.fillRect(this.x, this.y, 20, 20);
+    ctx.drawImage(ultraball, this.x, this.y, 40, 40);
   } else if (this.type === 'master') {
-    ctx.fillStyle = 'pink';
-    ctx.fillRect(this.x, this.y, 20, 20);
+    // ctx.fillStyle = 'pink';
+    // ctx.fillRect(this.x, this.y, 20, 20);
+    ctx.drawImage(masterball, this.x, this.y, 50, 50);
   }
 }
 
